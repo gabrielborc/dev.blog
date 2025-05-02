d6f9d25d74dea62775a12db74dc51c63ddfa847d
Gabriel Chiarelo <gabrielborc@gmail.com>
2025-05-01 23:32:24 -0300

    fix: adjusted render home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,8 @@
 import './home.css';
-import { Article } from '@/app/api/articles/data';
+import { articles } from '@/app/api/articles/data';
 import ArticleCard from '@/components/ArticleCard/ArticleCard';
-import Env from '@/config/environment';
-
 
 export default async function Home() {
-  const data = await fetch(`${Env.API_URL}/api/articles`);
-  const articles: Article[] = await data.json();
-
   return (
     <section className='list-articles'>
       {articles.map((article) => (
